a8dd86586fffa1fdde801cbf190007bc4885a7df
Maddie-Ledesma <144828460+Maddie-Ledesma@users.noreply.github.com>
2025-12-16 07:44:28 -0500

    feat: added recorded video, showing application features

---
+++ steps.sh
@@ -0,0 +1,6 @@
+
+1.- java -jar build/libs/simplechat-protocol-1.0.0-server.jar --config server-config.example.json
+2.- java -jar build/libs/simplechat-protocol-1.0.0-server.jar --config server-config.example.json
+3.- java -jar build/libs/simplechat-protocol-1.0.0-client.jar --host 127.0.0.1 --port 9000 --username alice
+4.- java -jar build/libs/simplechat-protocol-1.0.0-client.jar --host 127.0.0.1 --port 9000 --username alice
+5.- java -jar build/libs/simplechat-protocol-1.0.0-client.jar --host 127.0.0.1 --port 9000 --username mark
